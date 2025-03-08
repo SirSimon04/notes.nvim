18be0678e8afb9f671d00f6d29faea42dcce6eb6
SirSimon04 <simi@engelnetz.de>
2025-03-08 12:56:10 +0100

    refactor(none): Simplify notes setup

---
+++ lua/notes/init.lua
@@ -0,0 +1,20 @@
+local M = {}
+
+local config = {
+	books_dir = "~/notes/books",
+	dailies_dir = "~/notes/dailies",
+	meetings_dir = "~/notes/meetings",
+}
+
+function M.setup(opts)
+	opts = opts or {}
+	config = vim.tbl_deep_extend("force", config, opts)
+	-- config.books_dir = vim.fn.expand(config.books_dir)
+	-- config.dailies_dir = vim.fn.expand(config.dailies_dir)
+
+	require("notes.book_notes").setup(config)
+	require("notes.daily_notes").setup(config)
+	require("notes.meeting_notes").setup(config)
+end
+
+return M
